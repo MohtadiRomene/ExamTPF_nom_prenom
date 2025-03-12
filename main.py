3aec5cea915de791f087cec6ad8143077251f494
MohtadiRomene <>
2025-03-12 13:51:23 +0100

    modification du fichier python a nouveau

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ my_list = [1, 5, 7]
 print('La somme est :', sum(my_list))
 print('Le max est :', max(my_list))
 print('Le min est :', min(my_list))
+my_list = [1, 5, 7]
+if my_list:
+    print('La somme est :', sum(my_list))
+    print('Le max est :', max(my_list))
+    print('Le min est :', min(my_list))
+else:
+    print('Liste vide')
